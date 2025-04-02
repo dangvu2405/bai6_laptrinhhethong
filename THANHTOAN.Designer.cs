17a0394b5a61eebe35032f791e0992d16173e6a8
Đặng Thế Vũ <142129742+dangvu2405@users.noreply.github.com>
2025-04-02 20:00:12 +0700

    Add files via upload

---
+++ THANHTOAN.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace baitap_lon
+{
+    partial class THANHTOAN
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            textBox5 = new TextBox();
+            textBox4 = new TextBox();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            label8 = new Label();
+            label7 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            groupBox2 = new GroupBox();
+            dataGridViewPaymentDetails = new DataGridView();
+            groupBox3 = new GroupBox();
+            label11 = new Label();
+            button1 = new Button();
+            label10 = new Label();
+            label9 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewPaymentDetails).BeginInit();
+            groupBox3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(textBox5);
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label8);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Location = new Point(5, -2);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(400, 302);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Thông tin khách hàng";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(118, 257);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(258, 27);
+            textBox5.TabIndex = 9;
+            textBox5.TextChanged += textBox5_TextChanged;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(118, 188);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(258, 63);
+            textBox4.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(118, 92);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(258, 27);
+            textBox3.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(118, 136);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(258, 27);
+            textBox2.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(132, 47);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(244, 27);
+            textBox1.TabIndex = 5;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(13, 264);
+            label8.Name = "label8";
+            label8.Size = new Size(62, 20);
+            label8.TabIndex = 4;
+            label8.Text = "Voucher";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(15, 188);
+            label7.Name = "label7";
+            label7.Size = new Size(55, 20);
+            label7.TabIndex = 3;
+            label7.Text = "Địa chỉ";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(15, 139);
+            label6.Name = "label6";
+            label6.Size = new Size(97, 20);
+            label6.TabIndex = 2;
+            label6.Text = "Số điện thoại";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(15, 95);
+            label5.Name = "label5";
+            label5.Size = new Size(46, 20);
+            label5.TabIndex = 1;
+            label5.Text = "Email";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(15, 47);
+            label4.Name = "label4";
+            label4.Size = new Size(111, 20);
+            label4.TabIndex = 0;
+            label4.Text = "Tên khách hàng";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(dataGridViewPaymentDetails);
+            groupBox2.Location = new Point(411, -2);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(582, 313);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Giỏ hàng";
+            // 
+            // dataGridViewPaymentDetails
+            // 
+            dataGridViewPaymentDetails.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewPaymentDetails.Location = new Point(6, 26);
+            dataGridViewPaymentDetails.Name = "dataGridViewPaymentDetails";
+            dataGridViewPaymentDetails.RowHeadersWidth = 51;
+            dataGridViewPaymentDetails.Size = new Size(570, 276);
+            dataGridViewPaymentDetails.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(label11);
+            groupBox3.Controls.Add(button1);
+            groupBox3.Controls.Add(label10);
+            groupBox3.Controls.Add(label9);
+            groupBox3.Controls.Add(label3);
+            groupBox3.Controls.Add(label2);
+            groupBox3.Controls.Add(label1);
+            groupBox3.Location = new Point(5, 306);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(982, 200);
+            groupBox3.TabIndex = 2;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Thanh toán";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(469, 147);
+            label11.Name = "label11";
+            label11.Size = new Size(17, 20);
+            label11.TabIndex = 6;
+            label11.Text = "0";
+            label11.Click += label11_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(846, 154);
+            button1.Name = "button1";
+            button1.Size = new Size(130, 40);
+            button1.TabIndex = 0;
+            button1.Text = "Thanh toán";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(469, 101);
+            label10.Name = "label10";
+            label10.Size = new Size(17, 20);
+            label10.TabIndex = 5;
+            label10.Text = "0";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(469, 54);
+            label9.Name = "label9";
+            label9.Size = new Size(17, 20);
+            label9.TabIndex = 4;
+            label9.Text = "0";
+            label9.Click += label9_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(19, 54);
+            label3.Name = "label3";
+            label3.Size = new Size(58, 20);
+            label3.TabIndex = 3;
+            label3.Text = "Phí VAT";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(19, 101);
+            label2.Name = "label2";
+            label2.Size = new Size(93, 20);
+            label2.TabIndex = 2;
+            label2.Text = "Mã giảm giá";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(19, 147);
+            label1.Name = "label1";
+            label1.Size = new Size(147, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Tổng tiền thanh toán";
+            label1.Click += label1_Click;
+            // 
+            // THANHTOAN
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1005, 518);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "THANHTOAN";
+            Text = "Form3";
+            Load += THANHTOAN_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridViewPaymentDetails).EndInit();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private DataGridView dataGridViewPaymentDetails;
+        private GroupBox groupBox3;
+        private Button button1;
+        private Label label1;
+        private Label label3;
+        private Label label2;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private TextBox textBox5;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label8;
+        private Label label11;
+        private Label label10;
+        private Label label9;
+    }
+}
